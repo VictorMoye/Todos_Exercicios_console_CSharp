3e31794323c020010e278024d86f7c060dbaf759
victor moye <victormoye09@gmail.com>
2021-01-31 20:00:31 -0300

    adicionando um projeto simples de calcular um retangulo

---
+++ ProjetoRetangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProjetoRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Retangulo x = new Retangulo();
+
+            Console.Write("Informe a largura do retangulo: ");
+            x.Largura = int.Parse(Console.ReadLine());
+
+            Console.Write("/n Informe a Altura do retangulo: ");
+            x.Altura = int.Parse(Console.ReadLine());
+
+            Console.Write("Area = {0}", x.Area());
+            Console.WriteLine("Perimetro = {0}", x.Perimetro());
+            Console.WriteLine("Diagonal = {0}", x.Diadonal());
+        }
+    }
+}
---
+++ ProjetoRetangulo/Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoRetangulo
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura; 
+
+        public double Area()
+        {
+            return Largura * Altura; 
+        }
+        public double Perimetro()
+        {
+          return  2 * (Largura * Altura);
+        }
+        public double Diadonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}
